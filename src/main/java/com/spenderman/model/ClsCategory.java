6e2793092fc5957f86850047f93a3c1792508311
MohamedAyser1 <20240476@stud.fci-cu.edu.eg>
2026-04-28 16:35:20 +0300

    add data to ClsCategory and ClsCycle

--- src/main/java/com/spenderman/model/ClsCategory.java
+++ src/main/java/com/spenderman/model/ClsCategory.java
@@ -1,4 +1,59 @@
 package com.spenderman.model;
 
+import com.spenderman.model.StatusEnums.EnTransactionType;
+
 public class ClsCategory {
+    private int _categoryID;
+    private int _userID;
+    private String _name;
+    private String _hexColor;
+    private EnTransactionType _type;
+
+    public ClsCategory(int _categoryID, int _userID, String _name, String _hexColor,EnTransactionType _type) {
+        this._categoryID = _categoryID;
+        this._userID = _userID;
+        this._name = _name;
+        this._hexColor = _hexColor;
+        this._type = _type;
+    }
+
+    public EnTransactionType get_type() {
+        return _type;
+    }
+
+    public void set_type(EnTransactionType _type) {
+        this._type = _type;
+    }
+
+    public int get_categoryID() {
+        return _categoryID;
+    }
+
+    public void set_categoryID(int _categoryID) {
+        this._categoryID = _categoryID;
+    }
+
+    public int get_userID() {
+        return _userID;
+    }
+
+    public void set_userID(int _userID) {
+        this._userID = _userID;
+    }
+
+    public String get_name() {
+        return _name;
+    }
+
+    public void set_name(String _name) {
+        this._name = _name;
+    }
+
+    public String get_hexColor() {
+        return _hexColor;
+    }
+
+    public void set_hexColor(String _hexColor) {
+        this._hexColor = _hexColor;
+    }
 }
--- src/main/java/com/spenderman/model/ClsCycle.java
+++ src/main/java/com/spenderman/model/ClsCycle.java
@@ -1,4 +1,71 @@
 package com.spenderman.model;
+import com.spenderman.model.StatusEnums.EnCycleState;
+
+import java.time.LocalDate;
 
 public class ClsCycle {
+    private int _cycleID;
+    private int _userID;
+    private double _budgetAmount;
+    private LocalDate _startDate;
+    private LocalDate _endDate;
+    private EnCycleState _state;
+
+
+    public ClsCycle(int _userID, int _cycleID, double _budgetAmount, LocalDate _startDate, LocalDate _endDate,EnCycleState _state) {
+        this._userID = _userID;
+        this._cycleID = _cycleID;
+        this._budgetAmount = _budgetAmount;
+        this._startDate = _startDate;
+        this._endDate = _endDate;
+        this._state=_state;
+    }
+
+    public int get_userID() {
+        return _userID;
+    }
+
+    public void set_userID(int _userID) {
+        this._userID = _userID;
+    }
+
+    public int get_cycleID() {
+        return _cycleID;
+    }
+
+    public EnCycleState get_state() {
+        return _state;
+    }
+
+    public void set_state(EnCycleState _state) {
+        this._state = _state;
+    }
+
+    public void set_cycleID(int _cycleID) {
+        this._cycleID = _cycleID;
+    }
+
+    public double get_budgetAmount() {
+        return _budgetAmount;
+    }
+
+    public void set_budgetAmount(double _budgetAmount) {
+        this._budgetAmount = _budgetAmount;
+    }
+
+    public LocalDate get_startDate() {
+        return _startDate;
+    }
+
+    public void set_startDate(LocalDate _startDate) {
+        this._startDate = _startDate;
+    }
+
+    public LocalDate get_endDate() {
+        return _endDate;
+    }
+
+    public void set_endDate(LocalDate _endDate) {
+        this._endDate = _endDate;
+    }
 }
